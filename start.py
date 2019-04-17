9a4d80134edb710db2f01c13837c7f35eb88be01
Nicolas Pépin-Hermann <Nicolas.PepinHermann@gmail.com>
2019-04-17 08:45:41 +0000

    Fix start script

--- start.py
+++ start.py
@@ -4,14 +4,15 @@
 import os
 import sys
 
-sys.path.append('src/python')
+Install_Directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(Install_Directory)
+
+sys.path.append(Install_Directory +'/src/python')
 from library_loans_mailer import library_loans_mailer
 
 if __name__ == "__main__":
-    Install_Directory = os.path.dirname(os.path.abspath(__file__))
-
     mailer = library_loans_mailer(
-        preferences=Install_Directory + '/preferences.yaml',
-        working_directory=Install_Directory,
-        logging_directory=Install_Directory + '/log/')
+        preferences = Install_Directory + '/preferences.yaml',
+        working_directory = Install_Directory,
+        logging_directory = Install_Directory + '/log/')
     mailer.run()
